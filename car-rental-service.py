4dc09810f5a48c2eaaed9f41824efd023eede9d8
Rb-112594 <reboureslan@gmail.com>
2025-02-28 12:53:01 +0200

    Created subclass for daily rental calculation.

--- car-rental-service.py
+++ car-rental-service.py
@@ -6,10 +6,26 @@ class Vehicle:
         self.year = year
         self.dailyRental= dailyRental
 
-    def displayInfo(self):
-        
+    def displayInfo(self):        
         print (f"{category}: {self.brand} {self.model}, Year: {self.year}, Rental Price: ${self.dailyRental}/day.")
         
+class RentalCost(Vehicle):
+    # def superMetod(self):
+    #     super().__init__(self)
+    
+    def __init__ (self, brand, model, year, dailyRental, numberOfDays):
+        super().__init__ (brand, model, year, dailyRental)
+        self.days = numberOfDays
+        self.rentalcost = 0
+    
+    def calculateRent(self):
+        self.rentalCost = self.dailyRental*self.days
+        return self.rentalCost
+    
+    def displayInfo(self):
+        print (f"Rental cost for {self.brand} {self.model} for {numberOfDays} days: $ {self.rentalCost}")
+        
+
 
 
 
@@ -17,7 +33,7 @@ class Vehicle:
 
 
 # def promt ():
-#     vehicleDetails = Vehicle(input(f"Enter {category} brand:") , input(f"Enter {category} model: "),
+#     vehicleDetails = Vehicle(input(f"Enter {category} brand :") , input(f"Enter {category} model: "),
 #                             int(input(f"Enter {category} year: ")), float(input(f"Enter {category} daily rental price: $")))
 #     return vehicleDetails
 
@@ -25,7 +41,14 @@ class Vehicle:
 
 
 # # for the code
-# category = input ("Enter the vehicle type: ")
+category = input ("Enter the vehicle type: ")
+vehicleDetails = Vehicle(input(f"Enter {category} brand: ") , input(f"Enter {category} model: "), int(input(f"Enter {category} year: ")), float(input(f"Enter {category} daily rental price: $")))
+numberOfDays = int(input("Enter the number of days you wish to rent the car for: "))
 
+rental = RentalCost(vehicleDetails.brand, vehicleDetails.model, vehicleDetails.year, vehicleDetails.dailyRental, numberOfDays)
+
+rental.calculateRent()
+rental.displayInfo()
+# numberOfDays.displayInfo()
 # vehicleDetails = promt()
 # vehicleDetails.displayInfo()
