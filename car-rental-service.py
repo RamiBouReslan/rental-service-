b74006c4623cdd1fd6c22388a5a07a3503b69a0d
Rb-112594 <reboureslan@gmail.com>
2025-02-28 15:56:07 +0200

    Defined separate functions to perform tasks

--- car-rental-service.py
+++ car-rental-service.py
@@ -31,44 +31,86 @@ class RentalCost(Vehicle):
         return self.rentalCost
     
     def displayInfo(self):
-        print (f"Rental cost for {self.brand} {self.model} for {numberOfDays} days: $ {self.rentalCost}")
+        print (f"Rental cost for {self.brand} {self.model} for {self.days} days: $ {self.rentalCost}")
         
-class SeatCapacity(RentalCost):
+class SeatCapacity(Vehicle):
+
     def __init__ (self, brand, model, year, dailyRental, numberOfSeats):
         super().__init__ (brand, model, year, dailyRental)
         self.seats = numberOfSeats
 
     def displayInfo(self):        
-        print (f"{category}: {self.brand} {self.model}, Year: {self.year},Seats: {self.seats} Rental Price: ${self.dailyRental}/day.")
+        print (f"{category}: {self.brand} {self.model}, Year: {self.year},Seats: {self.seats} Rental Price: ${self.getDailyRental()}/day.")
 
-class EngineCapacity(RentalCost):
+class EngineCapacity(Vehicle):
+
     def __init__ (self, brand, model, year, dailyRental, enginePower):
         super().__init__ (brand, model, year, dailyRental)
         self.enginge = enginePower
 
     def displayInfo(self):        
-        print (f"{category}: {self.brand} {self.model}, Year: {self.year},Engine: {self.enginge} Rental Price: ${self.dailyRental}/day.")
+        print (f"{category}: {self.brand} {self.model}, Year: {self.year},Engine: {self.enginge} Rental Price: ${self.getDailyRental()}/day.")
 
-# def promt ():
-#     vehicleDetails = Vehicle(input(f"Enter {category} brand :") , input(f"Enter {category} model: "),
-#                             int(input(f"Enter {category} year: ")), float(input(f"Enter {category} daily rental price: $")))
-#     return vehicleDetails
+def transportation ():
+
+    vehicleDetails = Vehicle(input(f"Enter {category} brand :") ,
+                             input(f"Enter {category} model: ") ,
+                             int(input(f"Enter {category} year: ")),
+                             float(input(f"Enter {category} daily rental price: $")))
+    
+    return vehicleDetails
+
+def rentalPrice():
+        
+    numberOfDays = int(input(f"Enter the number of days you wish to rent the {category} for: "))
+
+    rental = RentalCost(vehicleDetails.brand, vehicleDetails.model, 
+                        vehicleDetails.year, vehicleDetails.getDailyRental(), 
+                        numberOfDays)
+    return rental
+
+
+def ifCar():
+    numberOfSeats = int(input("Enter the number of seats: "))
+
+    newVehicledetails = SeatCapacity (vehicleDetails.brand, vehicleDetails.model, 
+                        vehicleDetails.year, vehicleDetails.getDailyRental(), 
+                        numberOfSeats)
+
+    return newVehicledetails
+
+def ifBike():
+    enginePower = int(input("Enter the engin powe in 'cc': "))
+
+    newVehicledetails = EngineCapacity(vehicleDetails.brand, vehicleDetails.model, 
+                        vehicleDetails.year, vehicleDetails.getDailyRental(), 
+                        enginePower) 
+
+    return newVehicledetails
+
+def updatedPrice():
+
+    updatedRentalPrice = (float(input(f"Enter updated daily rental price: $")))
+
+    return rental.setDailyRental(updatedRentalPrice)
 
 
 
 
 # # for the code
-category = input ("Enter the vehicle type: ")
-vehicleDetails = Vehicle(input(f"Enter {category} brand: ") , input(f"Enter {category} model: "), int(input(f"Enter {category} year: ")), float(input(f"Enter {category} daily rental price: $")))
-numberOfDays = int(input("Enter the number of days you wish to rent the car for: "))
 
-rental = RentalCost(vehicleDetails.brand, vehicleDetails.model, vehicleDetails.year, vehicleDetails.getDailyRental(), numberOfDays)
-rental.calculateRent()
-rental.displayInfo()
 
-rental = RentalCost(vehicleDetails.brand, vehicleDetails.model, vehicleDetails.year, vehicleDetails.getDailyRental(), numberOfDays)
-rental.calculateRent()
-rental.displayInfo()
-# numberOfDays.displayInfo()
-# vehicleDetails = promt()
+
+
+# for i in range(len(vehicleList)):
+#     print(vehicleList[i])
+
+# rental.calculateRent()
+# rental.displayInfo()
+
+# rental = RentalCost(vehicleDetails.brand, vehicleDetails.model, vehicleDetails.year, vehicleDetails.getDailyRental(), numberOfDays)
+# rental.calculateRent()
+# rental.displayInfo()
+# # numberOfDays.displayInfo()
+
 # vehicleDetails.displayInfo()
